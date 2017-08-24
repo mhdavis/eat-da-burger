d389308841ae49bc84e3431b7f7fbad417b16b5d
Michael Davis <mhdavis1993@gmail.com>
2017-08-23 21:36:32 -0400

    burgers-controller.js: Wrote out core logic

---
+++ controllers/burgers-controller.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+let db = require("../models");
+
+// GET route for all of the posts
+router.get("/", function (req, res) {
+  db.Burger.findAll().then(function (data) {
+    res.json(data);
+    res.end();
+  });
+});
+
+// POST route for saving a new burger
+router.post("/", function (req, res) {
+  db.Burger.create(req.body).then(function (data) {
+    res.json(data);
+    res.end();
+  });
+});
+
+// PUT route for updating a burger's devoured status
+router.put("/", function (req, res) {
+  let burger = {
+    name: req.body.name,
+    devoured: !req.body.devoured
+  }
+
+  db.Burger.update(burger,
+     {
+       where: {
+         id: req.body.id
+       }
+     }).then(function(data) {
+       res.json(data);
+       res.end();
+     });
+});
+
+// DELETE route for deleting burger
+router.delete("/:id", function (req, res) {
+  db.Burger.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function (data) {
+    res.json(data);
+    res.end();
+  });
+});
+
+// Export routes for server.js to use
+module.exports = router;
